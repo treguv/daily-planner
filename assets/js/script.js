2f78d7368e901fcd87b07dc4dc39d3924cd1da2b
treguv <vladislavtregubov00@gmail.com>
2020-10-16 03:18:32 -0700

    Added ability to edit and save

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,68 +9,66 @@ function setCurrentDate(){
 }
 
 //Set up click listener for first row and replae p with text when clicked
-$(".event-1").on("click",function(){
+$(".row").on("click",".style-event",function(){
+    //Figure out which item was clicked
+    var rowId = $(this).attr("id").replace("event-","");
     //get current text
-    var currentText = $(".event-1 p").text().trim();
+    var target = "#event-"+ rowId+ " p";
+    var currentText = $(target).text().trim();
     //make new text input field
     var textInput = $("<textarea>")
-    .addClass("form-control ")
+    .addClass("form-control")
     .text(currentText);
     //replace 
-    $(".event-1 p").replaceWith(textInput);
+    $(target).replaceWith(textInput);
     console.log("clicked!"); 
+    textInput.trigger("focus");
 
 });
 
 //when you click off set back to text
-$(".event-1").on("blur", "textarea",function(){
+$(".row").on("blur", "textarea",function(){
     //get current text
-    console.log("blured text area");
-    var currentText = $(".event-1 textarea").text().trim();
+    var text = $(this).val().trim();
+    console.log(text);
+    //find closest row elemtn
+    var targetRow = $(this).closest(".style-event")
+    .attr("id").replace("event-","");
+    //Set the target to the text area
+    var currentTarget = "#event-" + targetRow + " textarea";
+    console.dir($(currentTarget));
     //make p tag again
     var pEl = $("<p>")
     .removeClass("form-control")
-    .text(currentText);
+    .text(text);
     //replace
-    $(".event-1 textarea").replaceWith(pEl);
+    console.log(currentTarget);
+    $(currentTarget).replaceWith(pEl);
 });
-
-$(".event-2").on("click",function(){
-    //get current text
-    var currentText = $(".event-2 p").text().trim();
-    //make new text input field
-    var textInput = $("<textarea>")
-    .addClass("form-control ")
-    .text(currentText);
-    //replace 
-    $(".event-2 p").replaceWith(textInput);
-    console.log("clicked!"); 
-
-});
-
-//when you click off set back to text
-$(".event-2").on("blur", "textarea",function(){
-    //get current text
-    console.log("blured text area");
-    var currentText = $(".event-2 textarea").text().trim();
-    //make p tag again
-    var pEl = $("<p>")
-    .removeClass("form-control")
-    .text(currentText);
-    //replace
-    $(".event-2 textarea").replaceWith(pEl);
+//Check for click of the save icon
+$(".row").on("click",".save-icon",function(){
+    //find the id of the row
+    var targetRow = $(this).attr("id").replace("save-","");
+    saveTasks(targetRow);
 });
 //Save tasks to storage
-function saveTasks(){
-    for (var i = 1; i < 10; i++){
-        var currentEvent = "#event-" + i + " p";
-        var currentEventEl =$(currentEvent);
-        console.log(currentEventEl.text());
-        // $(currentEventEl).text("This is elemnt " + i);
-        if((tasksForTheDay[i]) !== currentEventEl.text()){
-            tasksForTheDay[i-1] = currentEventEl.text();
-        }
-    }
+function saveTasks(targetRow){
+    //grab the row to save
+    var currentEvent = "#event-" + targetRow + " p";
+    //target the event
+    var currentEventEl =$(currentEvent);
+    //Update Text
+    console.log(currentEventEl.text());
+    tasksForTheDay[(parseInt(targetRow))-1] = currentEventEl.text();
+    // for (var i = 1; i < 10; i++){
+    //     var currentEvent = "#event-" + i + " p";
+    //     var currentEventEl =$(currentEvent);
+    //     console.log(currentEventEl.text());
+    //     // $(currentEventEl).text("This is elemnt " + i);
+    //     if((tasksForTheDay[i]) !== currentEventEl.text()){
+    //         tasksForTheDay[i-1] = currentEventEl.text();
+    //     }
+    // }
     localStorage.setItem("tasks",JSON.stringify(tasksForTheDay));
     console.log(tasksForTheDay.toString());
 }
@@ -97,6 +95,7 @@ function fillInEvents(){
         var currentEventEl =$(currentEvent);
         currentEventEl.text(tasksForTheDay[i-1] );
     }
+    loadTasks();
 }
 fillInEvents();
 setCurrentDate();
