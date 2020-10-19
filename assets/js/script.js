7d2a2a77ffb566893fd6b9d1ef528803b3a1110b
treguv <vladislavtregubov00@gmail.com>
2020-10-18 17:52:32 -0700

    Finished due date

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,7 @@ function setCurrentDate(){
 //Set up click listener for first row and replae p with text when clicked
 $(".row").on("click",".style-event",function(){
     //Figure out which item was clicked
-    console.log($(this));
+    //console.log($(this));
     var rowId = $(this).attr("id").replace("event-","");
     //get current text
     var target = "#event-"+ rowId+ " p";
@@ -21,8 +21,8 @@ $(".row").on("click",".style-event",function(){
     .addClass("form-control")
     .text(currentText);
     //replace 
-    $(target).replaceWith(textInput);
-    console.log("clicked!"); 
+    $(target).replaceWith(textInput);   
+    //console.log("clicked!"); 
     textInput.trigger("focus");
 
 });
@@ -31,7 +31,7 @@ $(".row").on("click",".style-event",function(){
 $(".row").on("blur", "textarea",function(){
     //get current text
     var text = $(this).val().trim();
-    console.log(text);
+    //console.log(text);
     //find closest row elemtn
     var targetRow = $(this).closest(".style-event")
     .attr("id").replace("event-","");
@@ -43,7 +43,7 @@ $(".row").on("blur", "textarea",function(){
     .removeClass("form-control")
     .text(text);
     //replace
-    console.log(currentTarget);
+    //console.log(currentTarget);
     $(currentTarget).replaceWith(pEl);
 });
 //Check for click of the save icon
@@ -52,6 +52,29 @@ $(".row").on("click",".save-icon",function(){
     var targetRow = $(this).attr("id").replace("save-","");
     saveTasks(targetRow);
 });
+//check this every hour
+var checkTimeOfTasks = setInterval(auditTime(), 3000);
+//Find the time slot
+function auditTime(){
+    console.log("In Audit Time!");
+    var currentTime =(moment().hour());
+    console.log(currentTime);
+    for(var i = 1; i <= 9; i++){
+        var checkingHour = i + 8;
+        var checkEvent = "#time-" + i + " p";
+        var eventTime = ($(checkEvent).text().replace("AM","").replace("PM",""));  
+        //check how far away the event is
+        //if event is in the past 
+        if(currentTime > checkingHour){
+            $(checkEvent).closest(".style-time").addClass("past-due");
+        }else if ((currentTime -checkingHour) === 0){
+            $(checkEvent).closest(".style-time").addClass("on-time");
+        }else if(currentTime < checkingHour){
+            $(checkEvent).closest(".style-time").addClass("future");
+        }
+    }
+}
+auditTime();
 //Save tasks to storage
 function saveTasks(targetRow){
     //grab the row to save
@@ -59,7 +82,7 @@ function saveTasks(targetRow){
     //target the event
     var currentEventEl =$(currentEvent);
     //Update Text
-    console.log(currentEventEl.text());
+    //console.log(currentEventEl.text());
     tasksForTheDay[(parseInt(targetRow))-1] = currentEventEl.text();
     // for (var i = 1; i < 10; i++){
     //     var currentEvent = "#event-" + i + " p";
@@ -71,13 +94,13 @@ function saveTasks(targetRow){
     //     }
     // }
     localStorage.setItem("tasks",JSON.stringify(tasksForTheDay));
-    console.log(tasksForTheDay.toString());
+    //console.log(tasksForTheDay.toString());
 }
 
 //Load the tasks
 function loadTasks(){
     //check if it has previously stored stuff
-    console.log(localStorage.getItem("tasks") !== null);
+    //console.log(localStorage.getItem("tasks") !== null);
     if(localStorage.getItem("tasks") !== null){
         //get the array from storage
         var arrayTask = localStorage.getItem("tasks");
